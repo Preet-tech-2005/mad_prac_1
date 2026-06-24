c5d3d61ade61151a6ed3710c56ae708e8ad46def
Hp <preetmitha58@gmail.com>
2026-06-24 12:35:50 +0530

    initial

---
+++ src/p1.kt
@@ -0,0 +1,30 @@
+fun main()
+{
+    var i=10
+    println("i=" +i)
+    var j:Int
+    j=20
+    val k=200
+
+
+    println("k=" +k)
+    println("j=" +j)
+    var l: Double
+    l=1.5
+    println("l=" +l)
+    var ch: Char = 'A'
+    println("ch=" +ch)
+    var p= "hello world"
+    println("message=" +p)
+    var q : Boolean
+    q= false
+    println("q=" +q)
+    var num: Long = 1234567890123
+    println("num=" +num)
+    var number: Short = 25
+    println("num=" +number)
+    var a=10
+    println("a=$a")
+    print(a)
+    
+}
---
+++ src/p12.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    val i=10000
+    val j:Double=i.toDouble()
+    println("i=" +i)
+    println("j=" +j)
+    val st="10"
+    val num: Int=st.toInt()
+    println("string=$num")
+
+
+}
