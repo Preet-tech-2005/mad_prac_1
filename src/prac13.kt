3057a7cb3120449ce89b784457be52c9f6c817b2
Hp <preetmitha58@gmail.com>
2026-07-01 11:36:35 +0530

    1/7/2026

---
+++ src/prac13.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    var s1:String ?="23"
+    var i:Int=s1!!.toInt()
+    println("i=" +i)
+
+    val s3=readln().toString()
+    println("Enrollment no=" +s3)
+    val s4=readln().toString()
+    println("student name=" +s4)
+    val s5=readln().toString()
+    println("Branch="+s5)
+    val s6=readln().toString()
+    println("Batch=" +s6)
+    val s7=readln().toString()
+    println("College name=" +s7)
+    val s8=readln().toString()
+    println("University name=" +s8)
+    val s9=readln().toInt()
+    println("Student age=" +s9)
+
+}
